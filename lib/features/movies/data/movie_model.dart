adaca8725b8a3a0eb14100e0510fc3185afd7dbe
Jordi Sabat <jsabat@gmail.com>
2020-10-27 13:05:43 +0100

    add Movie Model

---
+++ lib/features/movies/data/movie_model.dart
@@ -0,0 +1,75 @@
+import 'package:movie_app/features/movies/domain/entities/movie.dart';
+
+class MovieModel extends Movie {
+  MovieModel({
+    double popularity,
+    int voteCount,
+    bool video,
+    String posterPath,
+    int id,
+    bool adult,
+    String backdropPath,
+    String originalLanguage,
+    String originalTitle,
+    List<int> genreIds,
+    String title,
+    double voteAverage,
+    String overview,
+    String releaseDate,
+  }) : super(
+          popularity: popularity,
+          voteCount: voteCount,
+          video: video,
+          posterPath: posterPath,
+          id: id,
+          adult: adult,
+          backdropPath: backdropPath,
+          originalLanguage: originalLanguage,
+          originalTitle: originalTitle,
+          genreIds: genreIds,
+          title: title,
+          voteAverage: voteAverage,
+          overview: overview,
+          releaseDate: releaseDate,
+        );
+
+  factory MovieModel.fromJson(Map<String, dynamic> json) {
+    return MovieModel(
+      popularity: json['popularity'],
+      voteCount: json['vote_count'],
+      video: json['video'],
+      posterPath: json['poster_path'],
+      id: json['id'],
+      adult: json['adult'],
+      backdropPath: json['backdrop_path'],
+      originalLanguage: json['original_language'],
+      originalTitle: json['original_title'],
+      genreIds: json['genre_ids'] != null
+          ? new List<int>.from(json['genre_ids'])
+          : [],
+      title: json['title'],
+      voteAverage: json['vote_average'],
+      overview: json['overview'],
+      releaseDate: json['release_date'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'popularitaty': popularity,
+      'voteCount': voteCount,
+      'video': video,
+      'posterPath': posterPath,
+      'id': id,
+      'adult': adult,
+      'backdropPath': backdropPath,
+      'originalLanguage': originalLanguage,
+      'originalTitle': originalTitle,
+      'genreIds': genreIds,
+      'title': title,
+      'voteAverage': voteAverage,
+      'overview': overview,
+      'releaseDate': releaseDate,
+    };
+  }
+}
---
+++ test/features/movies/data/models/movie_model_test.dart
@@ -0,0 +1,75 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:movie_app/features/movies/data/movie_model.dart';
+import 'package:movie_app/features/movies/domain/entities/movie.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+void main() {
+  final tMovieModel = MovieModel(
+    popularity: 2700.167,
+    voteCount: 178,
+    video: false,
+    posterPath: "/7D430eqZj8y3oVkLFfsWXGRcpEG.jpg",
+    id: 528085,
+    adult: false,
+    backdropPath: "/5UkzNSOK561c2QRy2Zr4AkADzLT.jpg",
+    originalLanguage: "en",
+    originalTitle: "2067",
+    genreIds: [18, 878, 53],
+    title: "2067",
+    voteAverage: 5.6,
+    overview:
+        'A lowly utility worker is called to the future by a mysterious radio signal...',
+    releaseDate: "2020-10-01",
+  );
+
+  test(
+    'should be a subclass of Movie entity',
+    () async {
+      // assert
+      expect(tMovieModel, isA<Movie>());
+    },
+  );
+
+  test(
+    'should return a valid model',
+    () async {
+      // arrange
+      final Map<String, dynamic> jsonMap = json.decode(fixture('movie.json'));
+      // act
+      final result = MovieModel.fromJson(jsonMap);
+      // assert
+      expect(result, tMovieModel);
+    },
+  );
+
+  test(
+    'should return a JSON map containing the proper data',
+    () async {
+      // arrange
+      final expectedMap = {
+        "popularitaty": 2700.167,
+        "voteCount": 178,
+        "video": false,
+        "posterPath": "/7D430eqZj8y3oVkLFfsWXGRcpEG.jpg",
+        "id": 528085,
+        "adult": false,
+        "backdropPath": "/5UkzNSOK561c2QRy2Zr4AkADzLT.jpg",
+        "originalLanguage": "en",
+        "originalTitle": "2067",
+        "genreIds": [18, 878, 53],
+        "title": "2067",
+        "voteAverage": 5.6,
+        "overview":
+            "A lowly utility worker is called to the future by a mysterious radio signal...",
+        "releaseDate": "2020-10-01"
+      };
+      // act
+      final result = tMovieModel.toJson();
+      // assert
+      expect(result, expectedMap);
+    },
+  );
+}
